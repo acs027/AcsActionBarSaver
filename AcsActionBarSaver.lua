72b1e18c74340893ed1261d7a66769a90b7bbe6e
acs027 <alicihansarac@gmail.com>
2024-06-04 12:42:41 +0300

    v1.0.0

---
+++ AcsActionBarSaver.lua
@@ -0,0 +1,116 @@
+
+AcsActionBarSaverDB = {}
+
+local function changeActionBars(tab)
+    ClearCursor()
+    for i = 1, 180 do
+        if tab[i] ~= nil then 
+            local temp = tab[i]
+
+            if temp.type == "macro" then
+                PickupMacro(temp.id)
+                PlaceAction(i)
+            elseif temp.type == "spell" then
+                PickupSpell(temp.id)
+                PlaceAction(i)
+            elseif temp.type == "item" then
+                PickupItem(temp.id)
+                PlaceAction(i)
+            end
+        else
+            PickupAction(i)
+        end
+        ClearCursor()
+    end
+    print("Action bar setup loaded successfully")
+end
+
+local function printKeys(tab)
+    for k,v in pairs(tab) do
+        print(k)
+    end
+end
+
+local function saveActionBars(saveName)
+    local saveTable = {}
+    for i=1,180 do
+        local type, globID = GetActionInfo(i)
+        if type == "macro" then
+            local macroName, _, macroContent = GetMacroInfo(globID)
+            saveTable[i] = {type = type, id = macroName}
+        elseif (type == "spell" or type == "item") then
+            saveTable[i] = {type = type, id = globID}
+        end 
+    end
+    print("Action bar setup saved as "..saveName)
+    return saveTable
+end
+
+local function OnEvent(self, event, addOnName)
+    if addOnName == "AcsActionBarSaver" then
+        AcsActionBarSaverDB = AcsActionBarSaverDB or {}
+        print(addOnName.." Loaded!")
+    end
+end
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", OnEvent)
+
+SLASH_AABS1 = "/aabs"
+
+SlashCmdList.AABS = function(msg)
+    args = {}
+    count = 0
+    for word in msg:gmatch("%w+") do 
+        table.insert(args, word) 
+        count = count + 1
+    end
+
+    if count == 2 then
+        if args[1] == "save" then
+            if AcsActionBarSaverDB[args[2]] ~= nil then
+                print("You already have a setup saved with this name. Please choose a different name.")
+                return
+            end
+            AcsActionBarSaverDB[args[2]] = saveActionBars(args[2])
+            return
+            
+        elseif args[1] == "load" then
+            if AcsActionBarSaverDB[args[2]] == nil then
+                print("The specified setup does not exist. Please check the name and try again.")
+                return
+            end
+            changeActionBars(AcsActionBarSaverDB[args[2]])
+            return
+
+        elseif args[1] == "remove" then
+            if AcsActionBarSaverDB[args[2]] == nil then
+                print("The specified setup does not exist. Please check the name and try again.")
+                return
+            end
+            AcsActionBarSaverDB[args[2]] = nil
+            print("Setup "..args[2].." has been successfully removed!")
+            return
+        else
+            print("\nPlease use one of the following commands:\n" ..
+            "/aabs list\n" ..
+            "/aabs load *savename*\n" ..
+            "/aabs save *setname*\n" ..
+            "/aabs remove *savename*")
+        end
+
+    elseif msg == "list" then
+        print("---Saved Setups---")
+        printKeys(AcsActionBarSaverDB)
+        print("---------------------")
+    else
+        print("\nPlease use one of the following commands:\n" ..
+            "/aabs list\n" ..
+            "/aabs load *savename*\n" ..
+            "/aabs save *setname*\n" ..
+            "/aabs remove *savename*")
+    end
+end
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+Introducing the ultimate convenience for WoW Classic players: the Action Bar Saver addon! Say goodbye to tedious reconfigurations of your action bars every time you need a change. With just a few simple commands, you can effortlessly save and load your preferred action bar setups. Need to quickly switch between PvP and PvE setups? No problem. Want to streamline your raiding experience with different configurations for each encounter? Easy!
+
+Simply type '/aabs save savename' to store your current action bar layout, '/aabs load savename' to retrieve it when needed, '/aabs list' to see all your saved setups at a glance, and '/aabs remove savename' to tidy up your list. It's that straightforward!
+
+No more frustration, no more wasted time. Take control of your gameplay experience like never before with the Action Bar Saver addon for WoW Classic. Download now and experience the freedom of effortless customization!
